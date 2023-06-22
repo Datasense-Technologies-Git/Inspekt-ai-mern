1d1899c03a634858e6d35302ef222eeaf7224784
Dhilip <dhilip@datasense.in>
2023-06-22 10:10:29 +0530

    code deploying process

---
+++ root/config/config.js
@@ -0,0 +1,3 @@
+const accessTokenKey = "fd60b7a9-bd4f-4b36-9305-221f49f11e3d"
+
+module.exports = accessTokenKey
---
+++ root/models/inspectionModel.js
@@ -0,0 +1,88 @@
+const mongoose = require("mongoose");
+var validator = require("validator");
+const inspectionSchema = new mongoose.Schema({
+  project_name: {
+    type: String,
+    required: [true, "Project name required"],
+    validate(value) {
+      if (!validator.isLength(value, { min: 3, max: 64 })) {
+        throw Error("Length of the project name should be between 3-64");
+      }
+    },
+    trim: true,
+  },
+  inspection_id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  drone_operator: {
+    type: String,
+    required: true,
+  },
+  model_url_3D: {
+    type: String,
+    required: true,
+  },
+  Overview: {
+    total_defects: {
+      type: String,
+      required: true
+    },
+    construction_quality: {
+      type: String,
+      required: true
+    },
+    energy_loss: {
+      type: String,
+      required: true
+    },
+  },
+  urgency: {
+    urgent: {
+      type: String,
+      required: true
+    },
+    medium: {
+      type: String,
+      required: true
+    },
+    low: {
+      type: String,
+      required: true
+    },
+  },
+
+  title: {
+    safety_value: {
+      type: String,
+      required: true
+    },
+    utility_value: {
+      type: String,
+      required: true
+    },
+    regulatory_value: {
+      type: String,
+      required: true
+    },
+    asset_value: {
+      type: String,
+      required: true
+    },
+    cost_value: {
+      type: String,
+      required: true
+    },
+    operation_value: {
+      type: String,
+      required: true
+    },
+  },
+  n_Deleted: { type: Number, default: 1 },
+  dt_CreatedOn: { type: Date, default: Date.now },
+},{strict: false,versionKey: false});
+
+const Inspections = mongoose.model("Inspections", inspectionSchema, "inspections");
+
+module.exports = { Inspections };
